7a91a8bfb541cafc907dd1ac5b87c2c9b71b87d8
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2022-09-26 12:46:23 -0400

    :construction: WIP: extracting functions
    Extract super granular functions from within updateItem, before it goes
    into updateQuality2
    
    FSA22V2-97

--- src/gilded-rose/index.ts
+++ src/gilded-rose/index.ts
@@ -12,48 +12,87 @@ export class Item {
     this.quality = quality;
   }
 }
-// this is where the items.map would be
+function isNotStandardItem(item) {
+  return (item.name != 'Aged Brie' &&
+    item.name != 'Backstage passes to a TAFKAL80ETC concert' &&
+    item.name != 'Sulfuras, Hand of Ragnaros')
+};
+
+function isAgedBrie(item) {
+  return (item.name === 'Aged Brie')
+}
+function isBackstagePasses(item) {
+  return (item.name === 'Backstage passes to a TAFKAL80ETC concert')
+}
+function isSulfuras(item) {
+  return (item.name === 'Sulfuras, Hand of Ragnaros')
+}
+function isAnythingButSulfuras(item) {
+  return (item.name != 'Sulfuras, Hand of Ragnaros')
+}
+function isQualityStillLessThanFifty(item) {
+  return (item.quality < 50)
+}
+function isQualityStillMoreThanZero(item) {
+  return (item.quality > 0)
+}
+function isSellInBelowZero(item) {
+  return (item.sellIn < 0) 
+}
+function decreaseQuality(item, amount = 1) {
+  item.quality -= amount;
+}
+function decreaseQualityTwiceAsFast(item, amount = 2) {
+  decreaseQuality(item, 2)
+}
+function increaseQuality(item, amount = 1) {
+  item.quality += amount;
+}
+function decreaseSellIn(item, amount = 1) {
+  item.sell_in -= 1;
+}
+function zeroQuality (item) {
+  item.quality = item.quality - item.quality
+}
+
 function updateItem(item) {
-  if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-    if (item.quality > 0) {
-      if (item.name != 'Sulfuras, Hand of Ragnaros') {
-        item.quality = item.quality - 1
+  if (isNotStandardItem(item)) {
+        if (isQualityStillMoreThanZero(item)) {
+        decreaseQuality(item, 1)
       }
     }
-  } else {
-    if (item.quality < 50) {
-      item.quality = item.quality + 1
-      if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+   else {
+    if (isQualityStillLessThanFifty(item)) {
+      increaseQuality(item, 1);
+      if (isBackstagePasses(item)) {
         if (item.sellIn < 11) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1
+          if (isQualityStillLessThanFifty(item)) {
+            increaseQuality(item, 1);
           }
         }
         if (item.sellIn < 6) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1
+          if (isQualityStillLessThanFifty(item)) {
+            increaseQuality(item, 1);
           }
         }
       }
     }
   }
-  if (item.name != 'Sulfuras, Hand of Ragnaros') {
+  if (isAnythingButSulfuras(item)) {
     item.sellIn = item.sellIn - 1;
   }
-  if (item.sellIn < 0) {
-    if (item.name != 'Aged Brie') {
-      if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (item.quality > 0) {
-          if (item.name != 'Sulfuras, Hand of Ragnaros') {
-            item.quality = item.quality - 1
-          }
+  if (isSellInBelowZero(item)) {
+      if (isNotStandardItem(item)) {
+        if (isQualityStillMoreThanZero(item)) {
+            increaseQuality(item, 1);
         }
+      } else if (isAgedBrie(item)) {
+        increaseQuality(item, 1)
+      } else if (isBackstagePasses(item)) {
+        zeroQuality(item);
       } else {
-        item.quality = item.quality - item.quality
-      }
-    } else {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1
+      if (isQualityStillLessThanFifty(item)) {
+        increaseQuality(item, 1)
       }
     }
   }
