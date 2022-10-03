f9315c00f3168f2cebdb3558ce1cacd15e534c8e
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2022-10-03 11:43:28 -0400

    :recycle: refactor: WIP extract functions
    Continue to extract small functions and keep tests passing
    
    FSA22V2-97

--- src/gilded-rose/index.ts
+++ src/gilded-rose/index.ts
@@ -28,7 +28,9 @@ function isSulfuras(item) {
   return (item.name === 'Sulfuras, Hand of Ragnaros')
 }
 function isAnythingButSulfuras(item) {
-  return (item.name != 'Sulfuras, Hand of Ragnaros') 
+  if (item.name != 'Sulfuras, Hand of Ragnaros') {
+    return item.sellIn = item.sellIn - 1;
+  }
 }
 function isQualityStillLessThanFifty(item) {
   return (item.quality < 50)
@@ -49,115 +51,135 @@ function increaseQuality(item, amount = 1) {
   item.quality += amount;
 }
 function decreaseSellIn(item, amount = 1) {
-  item.sell_in -= 1;
+  item.sellIn -= 1;
 }
 function zeroQuality (item) {
   item.quality = item.quality - item.quality
-}
+  }
+
 function agedBrieQuality(item) {
-  // const { quality } = item;
-  // no matter what, decrease the sell_in
+  const { quality } = item;
   decreaseSellIn(item, 1);
-  if (isQualityStillMoreThanZero 
-      && isQualityStillLessThanFifty) {
+
+  // quality has to be between 0-50
+  if (quality >= 0 && quality < 50) {
     switch (true) {
-      // if the sell_in is negative (and quality is not exactly 50), increase the brie quality by 2
-      case (isSellInBelowZero):
-        // if(quality !== 50) {
-          increaseQuality(item, 2);
-          break;
-        // }
+      // if the sell_in is negative, increase the brie quality by 2
+      case item.sellIn <= 0:
+        increaseQuality(item, 2);
+        decreaseSellIn(item, 1);
+        break;
       // if the quality is between 0-50, increase the brie quality by 1
       default: 
-        // if(quality !== 50) {
-          increaseQuality(item, 1);
-          break;
-        // }
+        increaseQuality(item, 1);
+        break;
     }
   }
-}
+};
 function backstagePassesQuality(item) {
-  const { sell_in }= item;
+  const { sell_in } = item;
   // decrease the sell_in no matter what
   decreaseSellIn(item);
   // then check if the quality is less than 50
-  if(item.quality < 50){
-    switch (true) {
-      //increase the quality by 1 if the sell_in is between 11-50
-      case sell_in > 10: 
-        increaseQuality(item, 1);
-        break;
-      // //increase the quality by 2 if the sell_in is between 6-10
-      case sell_in > 5 && sell_in <= 10:
-        increaseQuality(item, 2);
-        break;
-      // //increase the quality by 3 if the sell_in is between 1-5
-      case sell_in > 0 && sell_in <= 5:
-        increaseQuality(item, 3);
-        break;
-      //decrease quality all the way if the sell_in is 0 or less
-      case sell_in <= 0: 
-        item.quality = item.quality - item.quality;
-        break;
+  if (isQualityStillLessThanFifty(item)) {
+    if (item.sellIn < 11) {
+      // if (isQualityStillLessThanFifty(item)) {
+        increaseQuality(item, 1)
+      }
+    
+    if (item.sellIn < 6) {
+    // then check if the quality is still less than 50
+      if (isQualityStillLessThanFifty(item)) {
+        increaseQuality(item, 1)
+      }
+    }
+    if (item.sellIn < 0) {
+      zeroQuality(item)
     }
   }
 }
-function standardItemChanges(item) {
+function standardItemUpdates(item) {
   // then check that the quality between 0-50, and decrease the quality by 1
   if (item.quality > 0 && item.quality < 50) {
     decreaseQuality(item, 1);
     // if the sell_in becomes negative, decrease quality again by 1
-    if(item.sell_in < 0) {
+    if(item.sellIn < 0) {
       decreaseQuality(item, 1);
     }
   }
 }
 
-
 function updateItem(item) {
-  if (isStandardItem(item)) {
-    if (isQualityStillMoreThanZero(item)) {
-    decreaseQuality(item, 1)
-    }
-  }
-   else {
-    if (isQualityStillLessThanFifty(item)) {
-      increaseQuality(item, 1);
-      if (isBackstagePasses(item)) {
-        if (item.sellIn < 11) {
-          if (isQualityStillLessThanFifty(item)) {
-            increaseQuality(item, 1);
-          }
-        }
-        if (item.sellIn < 6) {
-          if (isQualityStillLessThanFifty(item)) {
-            increaseQuality(item, 1);
-          }
-        }
-      }
-    }
-  }
-  if (isAnythingButSulfuras(item)) {
-    item.sellIn = item.sellIn - 1;
-  }
-  if (isSellInBelowZero(item)) {
-    if (isStandardItem(item)) {
-      if (isQualityStillMoreThanZero(item)) {
-          increaseQuality(item, 1);
-      }
-    } else if (isAgedBrie(item)) {
-      increaseQuality(item, 1)
-    } else if (isBackstagePasses(item)) {
-      zeroQuality(item);
-    } else {
-    if (isQualityStillLessThanFifty(item)) {
-      increaseQuality(item, 1)
-    }
-    }
-  }
-    return item;
+  // switch(item.name) {
+  //   case 'Aged Brie': 
+  //     agedBrieQuality(item);
+  //     break;
+  //   case 'Backstage passes to a TAFKAL80ETC concert':
+  //     backstagePassesQuality(item);
+  //     break;
+  //   case 'Sulfuras, Hand of Ragnaros':
+  //     break;
+  //   default: 
+  //     standardItemUpdates(item);
+  // }
+  //   return item;
 
+  // this if statement checks for a standard item, then decreases the sell_in & quality accordingly
+  if (isStandardItem(item)) {
+    standardItemUpdates(item);
+  } else {
+    if (isQualityStillLessThanFifty(item)) {
+      increaseQuality(item, 1)
+      if (isBackstagePasses(item)) {
+        backstagePassesQuality(item)
+      }
+    }
+  }
+  // this decreases the sell_in of every item EXCEPT sulfuras
+  isAnythingButSulfuras(item)
+  
+  if (isSellInBelowZero(item)) {
+    
+    if (item.name != 'Aged Brie') {
+      if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (isQualityStillMoreThanZero(item)) {
+          if (item.name != 'Sulfuras, Hand of Ragnaros') {
+            decreaseQuality(item, 1);
+
+          }
+        }
+      } else {
+        zeroQuality(item)
+      }
+    } 
+    else {
+      if (isQualityStillLessThanFifty(item)) {
+        increaseQuality(item, 1);
+      }
+    }
+  }
+  // if (isSellInBelowZero(item)) {
+  //   if (item.name != 'Aged Brie') {
+  //     if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+  //       if (isQualityStillMoreThanZero(item)) {
+  //         if (item.name != 'Sulfuras, Hand of Ragnaros') {
+  //           decreaseQuality(item, 1);
+
+  //         }
+  //       }
+  //     } else {
+  //       zeroQuality(item)
+  //     }
+  //   } 
+  //   else {
+  //     if (isQualityStillLessThanFifty(item)) {
+  //       increaseQuality(item, 1);
+  //     }
+  //   }
+  // }
+  return item;
 }
+
 
 // accumulative value of item?
 // items.reduce((item) => item decrease )
